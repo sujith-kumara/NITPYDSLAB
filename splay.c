1b49043a88e2e798ee7aed87e017a4e93636411c
sujithkumara <sujith.sunu19@gmail.com>
2023-09-15 06:56:27 +0000

    insert operation is fixed

--- splay.c
+++ splay.c
@@ -1,51 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
+struct Node {
     int key;
-    struct Node *left;
-    struct Node *right;
-} Node;
+    struct Node* left;
+    struct Node* right;
+};
 
-Node* newNode(int key) {
-    Node* node = (Node*)malloc(sizeof(Node));
-    node->key = key;
-    node->left = node->right = NULL;
-    return node;
-}
+typedef struct Node Node;
 
-Node* rightRotate(Node* x) {
-    Node* y = x->left;
-    x->left = y->right;
-    y->right = x;
-    return y;
-}
+// Function prototypes
+Node* createNode(int key);
+Node* splay(Node* root, int key);
+Node* insert(Node* root, int key);
+Node* delete(Node* root, int key);
+Node* leftRotate(Node* x);
+Node* rightRotate(Node* y);
+void inOrderTraversal(Node* root);
 
-Node* leftRotate(Node* x) {
-    Node* y = x->right;
-    x->right = y->left;
-    y->left = x;
-    return y;
+Node* createNode(int key) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Error: Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->key = key;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
 }
 
 Node* splay(Node* root, int key) {
-    if (root == NULL || root->key == key) return root;
-    if (root->key > key) {
-        if (root->left == NULL) return root;
-        if (root->left->key > key) {
+    if (root == NULL || root->key == key) {
+        return root;
+    }
+
+    if (key < root->key) {
+        if (root->left == NULL) {
+            return root;
+        }
+        if (key < root->left->key) {
             root->left->left = splay(root->left->left, key);
             root = rightRotate(root);
-        } else if (root->left->key < key) {
+        } else if (key > root->left->key) {
             root->left->right = splay(root->left->right, key);
-            if (root->left->right != NULL) root->left = leftRotate(root->left);
+            if (root->left->right != NULL) {
+                root->left = leftRotate(root->left);
+            }
         }
         return (root->left == NULL) ? root : rightRotate(root);
     } else {
-        if (root->right == NULL) return root;
-        if (root->right->key > key) {
+        if (root->right == NULL) {
+            return root;
+        }
+        if (key < root->right->key) {
             root->right->left = splay(root->right->left, key);
-            if (root->right->left != NULL) root->right = rightRotate(root->right);
-        } else if (root->right->key < key) {
+            if (root->right->left != NULL) {
+                root->right = rightRotate(root->right);
+            }
+        } else if (key > root->right->key) {
             root->right->right = splay(root->right->right, key);
             root = leftRotate(root);
         }
@@ -54,26 +67,40 @@ Node* splay(Node* root, int key) {
 }
 
 Node* insert(Node* root, int key) {
-    if (root == NULL) return newNode(key);
-    root = splay(root, key);
-    if (root->key == key) return root;
-    Node* node = newNode(key);
-    if (root->key > key) {
-        node->right = root;
-        node->left = root->left;
-        root->left = NULL;
-    } else {
-        node->left = root;
-        node->right = root->right;
-        root->right = NULL;
+    if (root == NULL) {
+        return createNode(key);
     }
-    return node;
+
+    root = splay(root, key);
+
+    if (key < root->key) {
+        Node* newNode = createNode(key);
+        newNode->right = root;
+        newNode->left = root->left;
+        root->left = NULL;
+        return newNode;
+    } else if (key > root->key) {
+        Node* newNode = createNode(key);
+        newNode->left = root;
+        newNode->right = root->right;
+        root->right = NULL;
+        return newNode;
+    } else {
+        return root;
+    }
 }
 
-Node* deleteNode(Node* root, int key) {
-    if (root == NULL) return NULL;
+Node* delete(Node* root, int key) {
+    if (root == NULL) {
+        return root;
+    }
+
     root = splay(root, key);
-    if (root->key != key) return root;
+
+    if (root->key != key) {
+        return root;
+    }
+
     Node* temp;
     if (root->left == NULL) {
         temp = root;
@@ -83,48 +110,74 @@ Node* deleteNode(Node* root, int key) {
         root = splay(root->left, key);
         root->right = temp->right;
     }
+
     free(temp);
     return root;
 }
 
-Node* search(Node* root, int key) {
-    return splay(root, key);
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    x->right = y->left;
+    y->left = x;
+    return y;
 }
 
-void inorder(Node* root) {
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    y->left = x->right;
+    x->right = y;
+    return x;
+}
+
+void inOrderTraversal(Node* root) {
     if (root != NULL) {
-        inorder(root->left);
+        inOrderTraversal(root->left);
         printf("%d ", root->key);
-        inorder(root->right);
+        inOrderTraversal(root->right);
     }
 }
 
 int main() {
     Node* root = NULL;
-    root = insert(root, 50);
-    root = insert(root, 30);
-    root = insert(root, 20);
-    root = insert(root, 40);
-    root = insert(root, 70);
-    root = insert(root, 60);
-    root = insert(root, 80);
-    
-    printf("Inorder traversal: ");
-    inorder(root);
-    printf("\n");
-    
-    root = deleteNode(root, 20);
-    printf("Inorder traversal after deleting 20: ");
-    inorder(root);
-    printf("\n");
-    
-    Node* searchResult = search(root, 30);
-    if (searchResult != NULL) {
-        printf("Key 30 found in the tree.\n");
-    } else {
-        printf("Key 30 not found in the tree.\n");
+    int choice, key;
+
+    while (1) {
+        printf("\nSplay Tree Menu:\n");
+        printf("1. Insert a key\n");
+        printf("2. Delete a key\n");
+        printf("3. Print in-order traversal\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the key to insert: ");
+                scanf("%d", &key);
+                root = insert(root, key);
+                printf("Splay Tree after insertion: ");
+                inOrderTraversal(root);
+                printf("\n");
+                break;
+            case 2:
+                printf("Enter the key to delete: ");
+                scanf("%d", &key);
+                root = delete(root, key);
+                printf("Splay Tree after deletion: ");
+                inOrderTraversal(root);
+                printf("\n");
+                break;
+            case 3:
+                printf("In-order traversal of splay tree: ");
+                inOrderTraversal(root);
+                printf("\n");
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
     }
-    
+
     return 0;
 }
-
