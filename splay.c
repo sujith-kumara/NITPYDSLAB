3b51d6309bca80f07c822a36c46c45def8a12731
sujithkumara <sujith.sunu19@gmail.com>
2023-09-22 02:42:30 +0000

    search operation added

--- splay.c
+++ splay.c
@@ -14,6 +14,7 @@ Node* createNode(int key);
 Node* splay(Node* root, int key);
 Node* insert(Node* root, int key);
 Node* delete(Node* root, int key);
+Node* search(Node* root, int key);
 Node* leftRotate(Node* x);
 Node* rightRotate(Node* y);
 void inOrderTraversal(Node* root);
@@ -129,6 +130,44 @@ Node* rightRotate(Node* y) {
     return x;
 }
 
+Node* search(Node* root, int key) {
+    if (root == NULL || root->key == key) {
+        return splay(root, key);
+    }
+
+    if (key < root->key) {
+        if (root->left == NULL) {
+            // Key not found, splay the last accessed node
+            return splay(root, key);
+        }
+        if (key < root->left->key) {
+            root->left->left = search(root->left->left, key);
+            root = rightRotate(root);
+        } else if (key > root->left->key) {
+            root->left->right = search(root->left->right, key);
+            if (root->left->right != NULL) {
+                root->left = leftRotate(root->left);
+            }
+        }
+        return (root->left == NULL) ? root : rightRotate(root);
+    } else {
+        if (root->right == NULL) {
+            // Key not found, splay the last accessed node
+            return splay(root, key);
+        }
+        if (key < root->right->key) {
+            root->right->left = search(root->right->left, key);
+            if (root->right->left != NULL) {
+                root->right = rightRotate(root->right);
+            }
+        } else if (key > root->right->key) {
+            root->right->right = search(root->right->right, key);
+            root = leftRotate(root);
+        }
+        return (root->right == NULL) ? root : leftRotate(root);
+    }
+}
+
 void inOrderTraversal(Node* root) {
     if (root != NULL) {
         inOrderTraversal(root->left);
@@ -143,10 +182,7 @@ int main() {
 
     while (1) {
         printf("\nSplay Tree Menu:\n");
-        printf("1. Insert a key\n");
-        printf("2. Delete a key\n");
-        printf("3. Print in-order traversal\n");
-        printf("4. Exit\n");
+        printf("1. Insert a key\n2. Delete a key\n3. Print in-order traversal\n4. Search for a key\n5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -173,6 +209,14 @@ int main() {
                 printf("\n");
                 break;
             case 4:
+                printf("Enter the key to search: ");
+                scanf("%d", &key);
+                root = search(root, key);
+                printf("Splay Tree after search: ");
+                inOrderTraversal(root);
+                printf("\n");
+                break;
+            case 5:
                 exit(0);
             default:
                 printf("Invalid choice. Please try again.\n");
